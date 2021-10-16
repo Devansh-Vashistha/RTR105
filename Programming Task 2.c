1dcc8402de1e109f3f1769b2ddcb006d268bc1a7
Devansh-Vashistha <89969531+Devansh-Vashistha@users.noreply.github.com>
2021-10-16 16:40:44 +0300

    Create Programming Task 2.c

---
+++ Programming Task 2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+void main()
+{
+    int a,b;
+    char c;
+    printf("please enter 1 number \n");
+    scanf("%c",&c);
+    printf("\n");
+    b = 9;
+  
+    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
+    a = a>>(b-1);
+    b = b-1;
+    printf("%d",a);
+    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
+    a = a>>(b-1);
+    b = b-1;
+    printf("%d",a);
+    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
+    a = a>>(b-1);
+    b = b-1;
+    printf("%d",a);
+    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
+    a = a>>(b-1);
+    b = b-1;
+    printf("%d",a);
+    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
+    a = a>>(b-1);
+    b = b-1;
+    printf("%d",a);
+    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
+    a = a>>(b-1);
+    b = b-1;
+    printf("%d",a);
+    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
+    a = a>>(b-1);
+    b = b-1;
+    printf("%d",a);
+    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
+    a = a>>(b-1);
+    b = b-1;
+    printf("%d",a);
+    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
+    a = a>>(b-1);
+    b = b-1;
+    printf("%d",a);
+}
