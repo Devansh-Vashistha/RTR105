74c32f773548cb032ab6cfc4d68fe727ce0522d6
Devansh-Vashistha <89969531+Devansh-Vashistha@users.noreply.github.com>
2021-12-22 22:16:49 +0200

    Create Programming Task 5.c

---
+++ Programming Task 5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main ()
+{
+  int a, b, c,i;
+  long long d,e;
+  a = 1;
+  e=1;
+  b = 1;
+  printf ("please enter the type of datatype that you are entering \n");
+  printf ("please enter 1 for int datatype  \n");
+  printf ("please enter 2 for char datatype  \n");
+  printf ("please enter 3 for long long datatype  \n");
+  scanf ("%d", &c);
+  
+    if(c==2){
+      printf("Sorry The factorial is not possible");
+      return 0;
+    }
+    else if(c==1){
+      printf ("please enter the number \n");
+      scanf ("%d", &c);
+      for(i=1;i<=c;i++){
+        a=a*i;
+        }
+    printf("The Factorial of %d is : %d",c,a);
+    }
+    else if(c==3){
+      printf ("please enter the number \n");
+      scanf ("%lld", &d);
+      for(i=1;i<=d;i++){
+        e=e*i;
+      }
+    printf("The Factorial of %lld is : %lld",d,e);
+    }
+
+return 0;
+}
