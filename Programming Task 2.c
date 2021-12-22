e30dae3913b6b1bb4e7d5e88a5f9b0e75ea18017
Devansh-Vashistha <89969531+Devansh-Vashistha@users.noreply.github.com>
2021-12-22 20:11:20 +0200

    Update Programming Task 2.c

--- Programming Task 2.c
+++ Programming Task 2.c
@@ -1,47 +1,49 @@
 #include <stdio.h>
 void main()
 {
-    int a,b;
-    char c;
+    int a,b,c;
     printf("please enter 1 number \n");
-    scanf("%c",&c);
-    printf("\n");
-    b = 9;
-  
+    scanf("%d",&c);
+    printf("\n \n");
+    b = 7;
+    a = c>>b;
+    printf("%d",a);
+    
+    
     a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
     a = a>>(b-1);
     b = b-1;
     printf("%d",a);
+    
     a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
     a = a>>(b-1);
     b = b-1;
     printf("%d",a);
+    
+    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
+    a = a>>(b-1);
+    b = b-1;
+    printf("%d  ",a);
+    
     a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
     a = a>>(b-1);
     b = b-1;
     printf("%d",a);
+    
     a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
     a = a>>(b-1);
     b = b-1;
     printf("%d",a);
+    
     a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
     a = a>>(b-1);
     b = b-1;
     printf("%d",a);
+    
     a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
     a = a>>(b-1);
     b = b-1;
     printf("%d",a);
-    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
-    a = a>>(b-1);
-    b = b-1;
-    printf("%d",a);
-    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
-    a = a>>(b-1);
-    b = b-1;
-    printf("%d",a);
-    a = ((c>>(b-1))<<(b-1)) - ((c>>b)<<b);
-    a = a>>(b-1);
-    b = b-1;
-    printf("%d",a);
+    
+    return 0;
 }
