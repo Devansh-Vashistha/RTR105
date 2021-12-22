50b0e78bd7ab74cdf4422576b1d6a4935c66d914
Devansh-Vashistha <89969531+Devansh-Vashistha@users.noreply.github.com>
2021-12-22 21:27:21 +0200

    Create Programming Task 4.c

---
+++ Programming Task 4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main ()
+{
+  int a, b, c;
+  long long d;
+  a = 1;
+  b = 1;
+  printf ("please enter the type of datatype that you are entering \n");
+  printf ("please enter 1 for int datatype  \n");
+  printf ("please enter 2 for char datatype  \n");
+  printf ("please enter 3 for long long datatype  \n");
+  scanf ("%d", &c);
+  
+    if(c==2){
+      printf("Sorry The factorial is not possible");
+      return 0;
+    }
+    else if(c==1){
+      printf ("please enter the number \n");
+      scanf ("%d", &c);
+      while(b<=c)
+        {
+        a=a*b;
+        b=b+1;
+        }
+    printf("The Factorial of %d is : %d",c,a);
+    }
+    else if(c==3){
+      printf ("please enter the number \n");
+      scanf ("%lld", &d);
+      while(b<=d)
+        {
+        a=a*b;
+        b=b+1;
+        }
+    printf("The Factorial of %lld is : %lld",d,a);
+    }
+
+return 0;
+}
