3eb00d66059fbd11d907e68341ad0f12b0b656cc
Devansh-Vashistha <89969531+Devansh-Vashistha@users.noreply.github.com>
2021-12-30 15:07:45 +0200

    Update Programming Task 4.c

--- Programming Task 4.c
+++ Programming Task 4.c
@@ -13,71 +13,58 @@ int main ()
   b1 = 1;
   c1 = 1;
   d1 = 1;
-  
   printf ("please enter the type of datatype that you are entering \n");
   printf ("please enter 1 for int datatype  \n");
   printf ("please enter 2 for char datatype  \n");
   printf ("please enter 3 for long long datatype  \n");
   scanf ("%d", &a);
   
-  
-    if(a==1){
+  if(a==1){
       printf ("please enter the number \n");
       scanf ("%d", &a);
-      while(b<=a)
-        {
+      for(b=1;b<=a;b++){
         c=c*b;
-        b=b+1;
-        }
-      b=1;
-      while(b<=(a-1))
-        {
+      }
+      b = 1;
+      for(b=1;b<=(a-1);b++){
         d=d*b;
-        b=b+1;
-        }
+      }
       if(c/d == a){
         printf("The Factorial of %d is : %d",a,c);
       }
       else{
           printf("The Factorial is not possible");
       }
-        
-    
-    }
-    else if(a==2){
+  }
+  
+  if(a==2){
       printf ("please enter the number \n");
       scanf ("%hhd", &a0);
-      while(b0<=a0)
-        {
+      for(b0=1;b0<=a0;b0++){
         c0=c0*b0;
-        b0=b0+1;
-        }
-      b0=1;
-      while(b0<=(a0-1))
-        {
+      }
+      b0 = 1;
+      for(b0=1;b0<=(a0-1);b0++){
         d0=d0*b0;
-        b0=b0+1;
-        }
+      }
       if(c0/d0 == a0){
         printf("The Factorial of %hhd is : %hhd",a0,c0);
       }
       else{
           printf("The Factorial is not possible");
       }
-    }
-    else if(a==3){
+  }
+  if(a==3){
       printf ("please enter the number \n");
       scanf ("%lld", &a1);
-      while(b1<=a1)
-        {
+      for(b1=1;b1<=a1;b1++){
         c1=c1*b1;
-        b1=b1+1;
-        }
-    printf("The Factorial of %lld is : %lld",a1,c1);
-    }
-    else{
-      printf("Sorry The factorial is not possible");
-      return 0;
-    }
-return 0;
+      }
+      b1 = 1;
+      for(b1=1;b1<=(a1-1);b1++){
+        d1=d1*b1;
+      }
+      printf("The Factorial of %lld is : %lld",a1,c1);
+      }
+  return 0;
 }
